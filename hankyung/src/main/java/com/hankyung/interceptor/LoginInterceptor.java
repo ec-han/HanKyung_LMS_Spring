fa322493d842923a958ca28beebe120f6659d44d
parkseonmi <seonmi0325@gmail.com>
2019-07-08 13:09:39 +0900

    1. 인터셉터 추가 - url에 경로를 직접 쳐서 들어올 시 member/update, /member/pwupdate, /member/delete, /lecture/wishView, /lecture/cartView 페이지로 검색하면 바로 로그인창 띄우게 수정

--- hankyung/src/main/java/com/hankyung/interceptor/LoginInterceptor.java
+++ hankyung/src/main/java/com/hankyung/interceptor/LoginInterceptor.java
@@ -22,48 +22,11 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		
 		if (session.getAttribute("id") == null) {
 			log.info("로그인 해주세요!!");
-			String referer = request.getHeader("referer");
-			String uri = request.getRequestURI();
-			String query = request.getQueryString();
-			
-			if (query == null || query.equals("null")) {
-				query = "";
-			} else {
-				query = "?" + query;
-			}
-			
-			// 0번지부터 시작함
-			int index = referer.lastIndexOf("/");
-			int length = referer.length();
-			String url = referer.substring(index, length);
-			log.info(">>>>> 수정된 URL : " + url);
-			
-			if (url.equals("/create")) {
-				response.sendRedirect(request.getContextPath() + "/board/list");
-				return false;
-			}
-			
-			// Login 페이지로 이동
-			FlashMap flashMap = RequestContextUtils.getOutputFlashMap(request);
-			flashMap.put("message", "nologin");
-			flashMap.put("uri", (uri+query));
-			log.info(">>>>> URI : " + (uri+query));
-			
-			RequestContextUtils.saveOutputFlashMap(referer, request, response);
-			response.sendRedirect(referer);
-			
+			response.sendRedirect(request.getContextPath() + "/member/login");
 			return false;
 		} else {
 			log.info(">>>>> 통과");
 			return true;
 		}
 	}
-	
-	// 메인액션 실행 된 후
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		super.postHandle(request, response, handler, modelAndView);
-	}
-	
 }
